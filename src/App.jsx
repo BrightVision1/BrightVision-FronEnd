d3f995770824a010e3503172476c3a8412937625
BrightVision1 <ayakhlife222@gmail.com>
2024-10-11 18:48:01 +0300

    edit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
+
+import './App.css'
+import Home from './pages/Home/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
 import Navbar from './components/NavBar/NavBar';
-import Banner from './components/Banner/Banner';
 import Footer from './components/Footer/Footer';
 import About from './pages/Home/About';
 import Services from './pages/Home/Services';
 import Projects from './pages/Home/Projects';
 import Client from './pages/Home/Client';
+import Banner from './components/Banner/Banner';
 import ContactUs from './pages/Home/ContactUs';
 
+// import About from './pages/About';
+// import Services from './pages/Services';
+// import Projects from './pages/Projects';
+// import Clients from './pages/Clients';
+// import Contact from './pages/Contact';
+// import SignIn from './pages/SignIn';
+// import Register from './pages/Register';
 function App() {
   return (
     <>
-      <Router basename="/BrightVision-FronEnd">
-        <Navbar />
-        <Banner/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/clients" element={<Client />} />
-          <Route path="/contact" element={<ContactUs />} />
-        </Routes>
-        <Footer/>
-      </Router>
+     <Router>
+      <Navbar />
+      <Banner/>
+      <Routes>
+        <Route path="/BrightVision-FronEnd/" element={<Home />} />
+       <Route path="/BrightVision-FronEnd/about" element={<About />} />
+        <Route path="/BrightVision-FronEnd/services" element={<Services />} />
+        <Route path="/BrightVision-FronEnd/projects" element={<Projects />} />
+        <Route path="/BrightVision-FronEnd/clients" element={<Client />} />
+        <Route path="/BrightVision-FronEnd/contact" element={<ContactUs />} />
+        {/* <Route path="/BrightVision-FronEnd/signin" element={<SignIn />} />
+        <Route path="/BrightVision-FronEnd/register" element={<Register />} /> */}
+      </Routes>
+      <Footer/>
+    </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
